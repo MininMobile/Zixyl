a3d9d19832792020c488019418ea1c667b22bb04
MininMobile <miningamer@gmail.com>
2017-11-13 19:14:01 +0000

    Updated boios

--- bot.js
+++ bot.js
@@ -1,14 +1,17 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const Discord = require("discord.js"),
+      fs = require('fs'),
+      client = new Discord.Client();
+
+const prefix = "x/";
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  log(`Logged in on ${client.guilds.size} servers!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.content === prefix + 'ping') {
+    msg.reply(':ping_pong: Pong');
   }
 });
 
-client.login('token');
+client.login(fs.readFile('/etc/hosts', 'utf8', function (err,data) { if (err) { return console.log(err); } return data; }));
