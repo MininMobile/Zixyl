4664968f7fcd290ee3628d16262de27940c256c4
MininMobile <miningamer@gmail.com>
2017-11-13 19:53:38 +0000

    now it works

--- bot.js
+++ bot.js
@@ -1,13 +1,22 @@
+// NODE.JS REQUIREMENTS
 const Discord = require("discord.js"),
       fs = require('fs'),
       bot = new Discord.Client();
 
+// VARIABLES
 const prefix = "x/";
 
+// FUNCTIONS
+function log(text) {
+  console.log(moment().format('LTS') + ' | ' + text);
+}
+
+// CONNECTION EVENTS
 bot.on('ready', () => {
   log(`Logged in on ${client.guilds.size} servers!`);
 });
 
+// ON MESSAGE
 bot.on('message', async message => {
   if (message.content === prefix + 'ping') {
     message.reply(':ping_pong: Pong');
