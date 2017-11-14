18e4e857564580e77475660fc4f22a809480173b
Hex <animation_plugs@outlook.com>
2017-11-14 22:08:10 +0000

    Added case's to make code compatible

--- Commands/Fun.js
+++ Commands/Fun.js
@@ -1,28 +1,31 @@
-if (commandIs('giveaway', message)) {
-    let perm = message.channel.permissionsFor(message.member).has('MENTION_EVERYONE') || message.author.id == 189400912333111297;
-    if (perm == false) {message.channel.send("Error: You need to have the Permission: Mention Everyone"); return;}
-    if (!slasharg[1] || !slasharg[2]) {message.channel.send("Error: `You gotta put in -giveaway/[title]/[description]`"); return;}
-    if (!slasharg[3]) {time = 300000} else {time = slasharg[3]*60000}
-      var embeded = new Discord.RichEmbed()
-            .setAuthor(message.author.username, message.author.avatarURL)
-            .setTitle("")
-            .addField(slasharg[1], slasharg[2])
-            .setFooter("Press the reaction below to enter.");
-    var m = await message.channel.send(embeded);
-    m.react("🎉");
-    setTimeout(function(){
-      var memberzz = "ERROR";
-      m.clearReactions();
-      var embeded = new Discord.RichEmbed()
-              .setAuthor(message.author.username, message.author.avatarURL)
-              .setTitle("")
-              .addField("Giveaway over.", "Congratulations to: " + memberzz + " for winning " + slasharg[1])
-              .setFooter("We collected " +  + " entries.");
-      m.edit(embeded);
-  }, time);
-  }
+exports.play = async function (i) {
+    switch (i.args[0]) {
+        case "giveaway":
+          if (perm == false) {message.channel.send("Error: You need to have the Permission: Mention Everyone"); return;}
+          let perm = message.channel.permissionsFor(message.member).has('MENTION_EVERYONE') || message.author.id == 189400912333111297;
+          if (!slasharg[1] || !slasharg[2]) {message.channel.send("Error: `You gotta put in -giveaway/[title]/[description]`"); return;}
+          if (!slasharg[3]) {time = 300000} else {time = slasharg[3]*60000}
+          var embeded = new Discord.RichEmbed()
+                .setAuthor(message.author.username, message.author.avatarURL)
+                .setTitle("")
+                .addField(slasharg[1], slasharg[2])
+                .setFooter("Press the reaction below to enter.");
+                var m = await message.channel.send(embeded);
+                m.react("🎉");
+                setTimeout(function(){
+                  var memberzz = "ERROR";
+                  m.clearReactions();
+                  var embeded = new Discord.RichEmbed()
+                  .setAuthor(message.author.username, message.author.avatarURL)
+                  .setTitle("")
+                  .addField("Giveaway over.", "Congratulations to: " + memberzz + " for winning " + slasharg[1])
+                  .setFooter("We collected " +  + " entries.");
+                  m.edit(embeded);
+          }, time);
+        }
+        break;
 
-if (commandIs('vote', message)) { // Be able to vote with --vote [question]
+  case "vote":
     var words = '';
     for (var i = 0; i != args.length-1; i++) {
         words = words + ' ' + args[i + 1];
@@ -35,3 +38,6 @@ if (commandIs('vote', message)) { // Be able to vote with --vote [question]
     await m.react("👍");
     await m.react("👎");
   }
+  break;
+}
+}
