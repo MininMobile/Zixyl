5b99fcc0e2860f27604c0c3494a54028fe665044
MininMobile <miningamer@gmail.com>
2017-11-14 22:22:51 +0000

    Fixed indentation and errors

--- Commands/Fun.js
+++ Commands/Fun.js
@@ -1,43 +1,49 @@
 exports.play = async function (i) {
-    switch (i.args[0]) {
-        case "giveaway":
-          if (perm == false) {message.channel.send("Error: You need to have the Permission: Mention Everyone"); return;}
-          let perm = message.channel.permissionsFor(message.member).has('MENTION_EVERYONE') || message.author.id == 189400912333111297;
-          if (!slasharg[1] || !slasharg[2]) {message.channel.send("Error: `You gotta put in -giveaway/[title]/[description]`"); return;}
-          if (!slasharg[3]) {time = 300000} else {time = slasharg[3]*60000}
-          var embeded = new Discord.RichEmbed()
-                .setAuthor(message.author.username, message.author.avatarURL)
-                .setTitle("")
-                .addField(slasharg[1], slasharg[2])
-                .setFooter("Press the reaction below to enter.");
-                var m = await message.channel.send(embeded);
-                m.react("🎉");
-                setTimeout(function(){
-                  var memberzz = "ERROR";
-                  m.clearReactions();
-                  var embeded = new Discord.RichEmbed()
-                  .setAuthor(message.author.username, message.author.avatarURL)
-                  .setTitle("")
-                  .addField("Giveaway over.", "Congratulations to: " + memberzz + " for winning " + slasharg[1])
-                  .setFooter("We collected " +  + " entries.");
-                  m.edit(embeded);
-          }, time);
-        }
-        break;
+  switch (i.args[0]) {
+    case "giveaway":
+      if (perm == false) { 
+        message.channel.send("Error: You need to have the Permission: Mention Everyone");
+        return
+      }
 
-  case "vote":
-    var words = '';
-    for (var i = 0; i != args.length-1; i++) {
+      let perm = message.channel.permissionsFor(message.member).has('MENTION_EVERYONE') || message.author.id == 189400912333111297;
+
+      if (!slasharg[1] || !slasharg[2]) {
+        message.channel.send("Error: `You gotta put in -giveaway/[title]/[description]`");
+        return
+      }
+      if (!slasharg[3]) { time = 300000; } else { time = slasharg[3]*60000; }
+      var embeded = new Discord.RichEmbed()
+        .setAuthor(message.author.username, message.author.avatarURL)
+        .setTitle("")
+        .addField(slasharg[1], slasharg[2])
+        .setFooter("Press the reaction below to enter.");
+      var m = await message.channel.send(embeded);
+      m.react("🎉");
+
+      setTimeout(function(){
+        var memberzz = "ERROR";
+        m.clearReactions();
+        var embeded = new Discord.RichEmbed()
+        .setAuthor(message.author.username, message.author.avatarURL)
+        .setTitle("")
+        .addField("Giveaway over.", "Congratulations to: " + memberzz + " for winning " + slasharg[1])
+        .setFooter("We collected " +  + " entries.");
+        m.edit(embeded);
+      }, time);
+    break
+    case "vote":
+      var words = '';
+      for (var i = 0; i != args.length-1; i++) {
         words = words + ' ' + args[i + 1];
       }
-    var embeded = new Discord.RichEmbed()
-            .setAuthor(message.author.username, message.author.avatarURL)
-            .addField("It's time to vote!", words)
-            .setFooter("");
-    m = await message.channel.send(embeded);
-    await m.react("👍");
-    await m.react("👎");
+      var embeded = new Discord.RichEmbed()
+        .setAuthor(message.author.username, message.author.avatarURL)
+        .addField("It's time to vote!", words)
+        .setFooter("");
+      m = await message.channel.send(embeded);
+      await m.react("👍");
+      await m.react("👎");
+    break
   }
-  break;
-}
 }
