7e47a98e6a6a554d2191873edb10661851072a82
MininMobile <miningamer@gmail.com>
2017-11-14 22:25:22 +0000

    fixed more errors
    reordered config

--- Commands/Fun.js
+++ Commands/Fun.js
@@ -2,30 +2,30 @@ exports.play = async function (i) {
   switch (i.args[0]) {
     case "giveaway":
       if (perm == false) { 
-        message.channel.send("Error: You need to have the Permission: Mention Everyone");
+        i.msg.channel.send("Error: You need to have the Permission: Mention Everyone");
         return
       }
 
-      let perm = message.channel.permissionsFor(message.member).has('MENTION_EVERYONE') || message.author.id == 189400912333111297;
+      let perm = i.msg.channel.permissionsFor(i.msg.member).has('MENTION_EVERYONE') || i.msg.author.id == 189400912333111297;
 
       if (!slasharg[1] || !slasharg[2]) {
-        message.channel.send("Error: `You gotta put in -giveaway/[title]/[description]`");
+        i.msg.channel.send("Error: `You gotta put in -giveaway/[title]/[description]`");
         return
       }
       if (!slasharg[3]) { time = 300000; } else { time = slasharg[3]*60000; }
       var embeded = new Discord.RichEmbed()
-        .setAuthor(message.author.username, message.author.avatarURL)
+        .setAuthor(i.msg.author.username, i.msg.author.avatarURL)
         .setTitle("")
         .addField(slasharg[1], slasharg[2])
         .setFooter("Press the reaction below to enter.");
-      var m = await message.channel.send(embeded);
+      var m = await i.msg.channel.send(embeded);
       m.react("🎉");
 
       setTimeout(function(){
         var memberzz = "ERROR";
         m.clearReactions();
         var embeded = new Discord.RichEmbed()
-        .setAuthor(message.author.username, message.author.avatarURL)
+        .setAuthor(i.msg.author.username, i.msg.author.avatarURL)
         .setTitle("")
         .addField("Giveaway over.", "Congratulations to: " + memberzz + " for winning " + slasharg[1])
         .setFooter("We collected " +  + " entries.");
@@ -34,14 +34,14 @@ exports.play = async function (i) {
     break
     case "vote":
       var words = '';
-      for (var i = 0; i != args.length-1; i++) {
-        words = words + ' ' + args[i + 1];
+      for (var i = 0; i != i.args.length-1; i++) {
+        words = words + ' ' + i.args[i + 1];
       }
       var embeded = new Discord.RichEmbed()
-        .setAuthor(message.author.username, message.author.avatarURL)
+        .setAuthor(i.msg.author.username, i.msg.author.avatarURL)
         .addField("It's time to vote!", words)
         .setFooter("");
-      m = await message.channel.send(embeded);
+      m = await i.msg.channel.send(embeded);
       await m.react("👍");
       await m.react("👎");
     break
